ebaed8a2bbf8a930a68cb57531aff7611a6b02bb
Halister Fernando <halisterfernando@hotmail.com>
2023-05-22 18:14:32 -0300

    feat: :zap: configurada rotas da aplicação

---
+++ src/Routes.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      {/* <Route path="/foods" element={<Food />} />
+      <Route path="/drinks" element={<Drink />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/done-recipes" element={<DoneRecipes />} />
+      <Route path="/favorite-recipes" element={<FavoriteRecipes />} />
+      <Route path="/foods/:id" element={<FoodDetails />} />
+      <Route path="/drinks/:id" element={<DrinkDetails />} />
+      <Route path="/foods/:id/in-progress" element={<FoodProgress />} />
+      <Route path="/drinks/:id/in-progress" element={<DrinkProgress />} />
+      <Route path="/explore/drinks" element={<ExploreDrinks />} />
+      <Route path="/explore/foods" element={<ExploreFoods />} />
+      <Route path="/explore/foods/ingredients" element={<FoodIngredients />} />
+      <Route path="/explore/drinks/ingredients" element={<DrinkIngredients />} />
+      <Route path="/explore/foods/nationalities" element={<FoodNationalities />} />
+      <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
