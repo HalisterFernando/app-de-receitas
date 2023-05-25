92335ee31c9387f7ad9861911740dd02e6caa41a
Halister Fernando <halisterfernando@hotmail.com>
2023-05-25 18:29:07 -0300

    refactor: :recycle: rotas atualizadas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,11 @@ import Food from './pages/Food/Food';
 import FoodDetails from './pages/Food/FoodDetails';
 import FoodInProgress from './pages/Food/FoodInProgress';
 import DoneRecipes from './pages/DoneRecipes';
+import Explore from './pages/Explore/Explore';
+import ExploreFoods from './pages/Explore/ExploreFoods';
+import ExploreDrinks from './pages/Explore/ExploreDrinks';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Profile from './pages/Profile';
 
 export default function AppRoutes() {
   return (
@@ -13,14 +18,14 @@ export default function AppRoutes() {
       <Route path="/foods/:id" element={ <FoodDetails /> } />
       <Route path="/foods/:id/in-progress" element={ <FoodInProgress /> } />
       <Route path="/done-recipes" element={ <DoneRecipes /> } />
+      <Route path="/explore" element={ <Explore /> } />
+      <Route path="/explore/foods" element={ <ExploreFoods /> } />
+      <Route path="/explore/drinks" element={ <ExploreDrinks /> } />
+      <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
+      <Route path="/profile" element={ <Profile /> } />
       {/* <Route path="/drinks" element={<Drink />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/favorite-recipes" element={<FavoriteRecipes />} />
       <Route path="/drinks/:id" element={<DrinkDetails />} />
       <Route path="/drinks/:id/in-progress" element={<DrinkProgress />} />
-      <Route path="/explore/drinks" element={<ExploreDrinks />} />
-      <Route path="/explore/foods" element={<ExploreFoods />} />
       <Route path="/explore/foods/ingredients" element={<FoodIngredients />} />
       <Route path="/explore/drinks/ingredients" element={<DrinkIngredients />} />
       <Route path="/explore/foods/nationalities" element={<FoodNationalities />} />
