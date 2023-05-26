dd17d48fb494f5feedd8bdd4c1868b5c83ffe17c
Halister Fernando <halisterfernando@hotmail.com>
2023-05-26 09:31:29 -0300

    feat: :sparkles: página de explorar comidas por ingrediente

---
+++ src/pages/Explore/ExploreFoodIngredients.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import Header from '../../components/Header';
+import { AppContext } from '../../context/Provider';
+import IngredientCard from '../../components/Cards/IngredientCard';
+import useExplore from '../../hooks/useExplore';
+
+const IMAGE_URL = 'https://www.themealdb.com/images/ingredients/';
+
+export default function ExploreFoodIngredients() {
+  const { allIngredients } = useContext(AppContext);
+  useExplore();
+
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div
+        className="
+        flex
+        flex-wrap
+        justify-center
+        gap-2
+        items-center
+        h-full
+        overflow-y-scroll
+        py-2
+        "
+      >
+        {allIngredients.map(({ idIngredient, strIngredient }, index) => (
+          <IngredientCard
+            name={ strIngredient }
+            image={ `${IMAGE_URL}${strIngredient}.png` }
+            index={ index }
+            key={ idIngredient }
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
