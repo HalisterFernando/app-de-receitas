39be58cf659d5ccaec806a0559d7c01d548f43ae
Halister Fernando <halisterfernando@hotmail.com>
2023-05-27 13:12:16 -0300

    feat: :sparkles: requisição de bebida por id

--- src/services/drinkServices.js
+++ src/services/drinkServices.js
@@ -10,3 +10,30 @@ export async function fetchDrinks() {
     console.log(err);
   }
 }
+
+export async function fetchDrinkCategories() {
+  try {
+    const response = await axios.get(`${BASE_URL}/list.php?c=list`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function fetchDrinksByCategory(category) {
+  try {
+    const response = await axios.get(`${BASE_URL}/filter.php?c=${category}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function fetchDrinkById(id) {
+  try {
+    const response = await axios.get(`${BASE_URL}/lookup.php?i=${id}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
