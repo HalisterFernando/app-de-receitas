e673eec18696af227a4de34b3acd67048dc618a3
Halister Fernando <halisterfernando@hotmail.com>
2023-06-01 23:39:38 -0300

    test: :zap: testa chave criadas no localStorage após login e mudança de rota

--- src/test/login.test.jsx
+++ src/test/login.test.jsx
@@ -2,7 +2,6 @@ import { describe, it, expect, vi } from 'vitest';
 import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import useLogin from '../hooks/useLogin';
 
 describe('Login tests suites', () => {
   it('should render the form and handle the input changes', () => {
@@ -49,11 +48,19 @@ describe('Login tests suites', () => {
 
     expect(submitButton).toBeDisabled();
   });
-  it('should navigate to "/foods"', () => {
+  it('should create localStorage keys and navigate to "/foods"', () => {
     renderWithRouter(<App />);
+
+    fireEvent.change(screen.getByTestId('email-input'), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByTestId('password-input'), { target: { value: 'password123' } });
 
     fireEvent.submit(screen.getByTestId('login-submit-btn'));    
 
+    expect(localStorage.getItem('mealsToken')).toEqual('{}');
+    expect(localStorage.getItem('cocktailsToken')).toEqual('{}');
+    expect(localStorage.getItem('user')).toEqual('\"test@example.com\"');
+    
     expect(window.location.pathname).toBe('/foods');    
+
   });
 });
