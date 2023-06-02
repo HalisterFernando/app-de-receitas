efc31580a2897c00f2ea5761ba0cbde4064bc875
Halister Fernando <halisterfernando@hotmail.com>
2023-06-01 23:38:44 -0300

    feat: :zap: adicionado rodapé a página de explorar

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Explore() {
   const navigate = useNavigate();
@@ -25,6 +26,7 @@ export default function Explore() {
           Explore Drinks
         </button>
       </div>
+      <Footer />
     </div>
   );
 }
