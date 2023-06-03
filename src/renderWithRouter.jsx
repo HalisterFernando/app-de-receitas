517f2dd7677f6215eaac3fa7e4dfa46fb1d07de4
Halister Fernando <halisterfernando@hotmail.com>
2023-06-03 16:59:51 -0300

    test: :zap: iniciado testes para página de comidas

--- src/renderWithRouter.jsx
+++ src/renderWithRouter.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Provider from './context/Provider';
 
-export default function renderWithRouter(component) {
+export default function renderWithRouter(component, { route = '/' } = {}) {
+  window.history.pushState({}, 'App', route);
   return render(
     <BrowserRouter>
       <Provider>
