f084d9e01fc88deaa3808116e6ee3a604db9024d
Halister Fernando <halisterfernando@hotmail.com>
2023-06-15 16:36:17 -0300

    refactor: :art: estilização e lógica do componente passada para o custom hook

--- src/pages/Drink/DrinkInProgress.jsx
+++ src/pages/Drink/DrinkInProgress.jsx
@@ -1,17 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import clipboardCopy from 'clipboard-copy';
+import React, { useContext } from 'react';
+
 import { useParams } from 'react-router-dom';
 import useFavorite from '../../hooks/useFavorite';
 import { AppContext } from '../../context/Provider';
 import FavoriteBtn from '../../components/FavoriteBtn';
 import useInProgress from '../../hooks/useInProgress';
 import Alert from '../../components/Alert';
-import useTimeOut from '../../hooks/useTimeout';
+
 import useLoading from '../../hooks/useLoading';
 import Loading from '../../components/Loading';
 import useRecipeDetails from '../../hooks/useRecipeDetails';
 import useFinishedRecipes from '../../hooks/useFinishedRecipes';
 import ShareBtn from '../../components/ShareBtn';
+import useShare from '../../hooks/useShare';
 
 const TYPE = 'drinks';
 
@@ -20,110 +21,116 @@ export default function DrinkInProgress() {
   useRecipeDetails(id, TYPE);
   const { loading } = useLoading();
   const { currentRecipe: { recipe, ingredients },
-    favoriteRecipes,
     inProgressRecipes: { cocktails } } = useContext(AppContext);
-  const { addFavoriteRecipe, removeFavoriteRecipe, recipeToFavorite } = useFavorite(TYPE);
-  const { show, timeOut } = useTimeOut();
-  const [isFavorite, setIsFavorite] = useState('');
-  const { addIngredient, removeIngredient } = useInProgress(id, TYPE);
-  const { finishRecipe } = useFinishedRecipes(TYPE);
-
+  const { addFavoriteRecipe, removeFavoriteRecipe,
+    recipeToFavorite, isFavorite } = useFavorite(id);
+  const { handleUsedIngredients } = useInProgress(id, TYPE);
+  const { finishRecipe, isReadyToFinish } = useFinishedRecipes(id, TYPE);
+  const { show, handleShare } = useShare();
   const cocktailsForId = cocktails[id] || [];
 
-  const handleUsedIngredients = ({ target: { checked, value } }) => {
-    if (checked) {
-      addIngredient(value);
-    } else {
-      removeIngredient(value);
-    }
-  };
-
-  const handleShare = () => {
-    clipboardCopy(`http://localhost:3000/drinks/${id}`);
-    timeOut();
-  };
-
-  const handleFinish = () => ingredients.length !== cocktailsForId.length;
-
-  useEffect(() => {
-    setIsFavorite(favoriteRecipes.some((favorite) => favorite.id === id));
-  }, [favoriteRecipes]);
-
   return (
-    loading ? <Loading /> : (
-      <main className="food-progress-container">
-        <div>
-          <img
-            className="food-image"
-            data-testid="recipe-photo"
-            src={ recipe.strDrinkThumb }
-            alt="Foto da Receita"
-          />
-        </div>
-        <div className="flex items-center justify-between p-2">
-          <h4 data-testid="recipe-title">{recipe.strDrink}</h4>
-          <div className="flex gap-3">
-            <ShareBtn handleShare={ handleShare } />
-            <FavoriteBtn
-              recipe={ recipeToFavorite(recipe, TYPE) }
-              add={ addFavoriteRecipe }
-              remove={ removeFavoriteRecipe }
-              isFavorite={ isFavorite }
+    <div className="flex flex-col h-screen bg-orange-200">
+      {loading ? <Loading /> : (
+        <main className="overflow-y-scroll animate-slide-up">
+          <div>
+            <img
+              className="food-image"
+              data-testid="recipe-photo"
+              src={ recipe.strDrinkThumb }
+              alt="Foto da Receita"
             />
           </div>
-        </div>
-        <span data-testid="recipe-category" className="p-2">Categoria</span>
-        {show && <Alert show={ show } />}
-        <div className="p-2">
-          <h4>Ingredientes</h4>
-          <div className="flex flex-col p-2 bg-orange-400 rounded">
-            {ingredients.map((ingredient, idx) => (
-              <label
-                data-testid={ `${idx}-ingredient-step` }
-                htmlFor={ ingredient }
-                key={ ingredient }
-                className={ `${cocktailsForId.includes(ingredient)
-                  ? 'line-through' : ''}` }
+          <div className="p-2 m-2 bg-white rounded-xl">
+            <div
+              className="
+                flex
+                items-center
+                justify-between
+                "
+            >
+              <h1
+                className="text-orange-600 text-xl font-semibold"
+                data-testid="recipe-title"
               >
-                <input
-                  type="checkbox"
-                  id={ id }
-                  name={ ingredient }
-                  value={ ingredient }
-                  checked={ cocktailsForId.includes(ingredient) }
-                  onChange={ (ev) => handleUsedIngredients(ev) }
+                {recipe.strDrink}
+              </h1>
+              <div className="flex gap-3">
+                <ShareBtn handleShare={ handleShare } />
+                <FavoriteBtn
+                  recipe={ recipeToFavorite(recipe, TYPE) }
+                  add={ addFavoriteRecipe }
+                  remove={ removeFavoriteRecipe }
+                  isFavorite={ isFavorite }
                 />
-                {` ${ingredient}`}
-              </label>
-            ))}
+              </div>
+            </div>
+            {show && <Alert show={ show } />}
+            <h4
+              className="text-sm text-slate-900 my-2"
+              data-testid="recipe-category"
+            >
+              { `Category - ${recipe.strCategory}` }
+            </h4>
+            <h3 className="font-semibold my-2 text-orange-600">Ingredients</h3>
+            <div className="flex flex-col gap-1 p-2">
+              {ingredients.map((ingredient, idx) => (
+                <label
+                  data-testid={ `${idx}-ingredient-step` }
+                  htmlFor={ ingredient }
+                  key={ ingredient }
+                  className={ `${cocktailsForId.includes(ingredient)
+                    ? 'line-through' : ''}` }
+                >
+                  <input
+                    type="checkbox"
+                    id={ id }
+                    name={ ingredient }
+                    value={ ingredient }
+                    checked={ cocktailsForId.includes(ingredient) }
+                    onChange={ (ev) => handleUsedIngredients(ev) }
+                    className="accent-orange-400"
+                  />
+                  {` ${ingredient}`}
+                </label>
+              ))}
+            </div>
+
+            <h3 className="font-semibold my-2 text-orange-600">Instructions</h3>
+            <p
+              data-testid="instructions"
+            >
+              { recipe.strInstructions }
+            </p>
+
           </div>
-        </div>
-        <div className="instructions-container">
-          <h4 className="p-2">Instruções</h4>
-          <p
-            data-testid="instructions"
-            className="bg-orange-400 p-2 m-2 rounded"
+
+          <button
+            type="button"
+            data-testid="finish-recipe-btn"
+            className={
+              `${isReadyToFinish() ? 'bg-orange-400' : 'bg-white'} 
+              p-1 
+              mx-auto 
+              block 
+              py-1 
+              px-2 
+              my-4
+              bg-orange-400 
+              rounded-full
+              shadow-md
+              shadow-black 
+              font-semibold
+            `
+            }
+            disabled={ !isReadyToFinish() }
+            onClick={ () => finishRecipe(recipe) }
           >
-            {recipe.strInstructions}
-          </p>
-        </div>
-        <button
-          type="button"
-          data-testid="finish-recipe-btn"
-          className={
-            `${handleFinish() ? 'bg-orange-200' : 'bg-orange-400'} 
-          p-1 
-          mx-auto 
-          block 
-          rounded my-4
-          `
-          }
-          disabled={ handleFinish() }
-          onClick={ () => finishRecipe(recipe) }
-        >
-          Finish Recipe
-        </button>
-      </main>
-    )
+            Finish Recipe
+          </button>
+        </main>
+      )}
+    </div>
+
   );
 }
