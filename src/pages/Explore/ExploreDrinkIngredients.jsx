23fe1ee20ebba247884c59f14245fc8fb19e15e6
Halister Fernando <halisterfernando@hotmail.com>
2023-06-22 15:04:29 -0300

    style: :art: estilizada página de explorar bebidas por ingredientes - inserido footer

--- src/pages/Explore/ExploreDrinkIngredients.jsx
+++ src/pages/Explore/ExploreDrinkIngredients.jsx
@@ -3,12 +3,15 @@ import Header from '../../components/Header';
 import { AppContext } from '../../context/Provider';
 import IngredientCard from '../../components/Cards/IngredientCard';
 import useExplore from '../../hooks/useExplore';
+import useCategoryFilters from '../../hooks/useCategoryFilters';
+import Footer from '../../components/Footer';
 
 const IMAGE_URL = 'https://www.thecocktaildb.com/images/ingredients/';
 
 export default function ExploreDrinkIngredients() {
   const { allIngredients } = useContext(AppContext);
   useExplore();
+  const { animateCard } = useCategoryFilters();
 
   return (
     <div className="h-screen flex flex-col">
@@ -23,6 +26,7 @@ export default function ExploreDrinkIngredients() {
         h-full
         overflow-y-scroll
         py-2
+        bg-orange-200
         "
       >
         {allIngredients.map(({ strIngredient1 }, index) => (
@@ -31,9 +35,11 @@ export default function ExploreDrinkIngredients() {
             image={ `${IMAGE_URL}${strIngredient1}-Medium.png` }
             index={ index }
             key={ strIngredient1 }
+            animate={ animateCard }
           />
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
