f6996391c93ae12b9ad02e08d11dcbfa6095825e
Halister Fernando <halisterfernando@hotmail.com>
2023-06-22 14:47:24 -0300

    feat: :zap: inserido footer em explorar comidas por nacionalidade

--- src/pages/Explore/ExploreFoodNationality.jsx
+++ src/pages/Explore/ExploreFoodNationality.jsx
@@ -4,6 +4,7 @@ import NationalityOptions from '../../components/NationalityOptions';
 import RecipeCard from '../../components/Cards/RecipeCard';
 import { AppContext } from '../../context/Provider';
 import useCategoryFilters from '../../hooks/useCategoryFilters';
+import Footer from '../../components/Footer';
 
 export default function ExploreFoodNationality() {
   const { meals } = useContext(AppContext);
@@ -36,6 +37,7 @@ export default function ExploreFoodNationality() {
           />
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
