507c99ffd8d2fc5261b77deda45118241474fdb0
Halister Fernando <halisterfernando@hotmail.com>
2023-06-27 15:49:09 -0300

    style: :art: responsividade do footer - refatorada lógica para mudança de cor dos botões

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import React from 'react';
 import { BiDrink, BiCompass } from 'react-icons/bi';
 import { GiKnifeFork } from 'react-icons/gi';
 import useFooter from '../hooks/useFooter';
 
-const SELECTED = 'rounded-full h-9 w-9 shadow-sm shadow-black text-white bg-black';
-const UNSELECTED = 'rounded-full h-9 w-9 shadow-sm shadow-black text-black bg-white';
 function Footer() {
   const { navigate, foodsPathname, drinksPathname, explorePathname } = useFooter();
   return (
@@ -12,7 +11,10 @@ function Footer() {
       className="
     flex
     justify-between
+    lg:justify-center
+    lg:gap-60
     p-1
+    px-2
     bg-gradient-to-t
     from-orange-400
     to-orange-200
@@ -21,42 +23,66 @@ function Footer() {
     >
       <button
         className={
-          `${drinksPathname ? SELECTED : UNSELECTED}
+          `${drinksPathname ? 'bg-black' : 'bg-white'}
           flex
           items-center
           justify-center
+          rounded-full
+          h-9
+          w-9
+          shadow-sm
+          shadow-black
           `
         }
         type="button"
         onClick={ () => navigate('/drinks') }
       >
-        <BiDrink data-testid="drinks-bottom-btn" className="text-2xl" />
+        <BiDrink
+          data-testid="drinks-bottom-btn"
+          className={ `text-2xl ${drinksPathname ? 'text-white' : 'text-black'}` }
+        />
       </button>
       <button
         className={
-          `${explorePathname ? SELECTED : UNSELECTED}
+          `${explorePathname ? 'bg-black' : 'bg-white'}
           flex
           items-center
           justify-center
-        `
+          rounded-full
+          h-9
+          w-9
+          shadow-sm
+          shadow-black
+          `
         }
         type="button"
         onClick={ () => navigate('/explore') }
       >
-        <BiCompass data-testid="explore-bottom-btn" className="text-2xl" />
+        <BiCompass
+          data-testid="explore-bottom-btn"
+          className={ `text-2xl ${explorePathname ? 'text-white' : 'text-black'}` }
+        />
       </button>
       <button
         className={
-          `${foodsPathname ? SELECTED : UNSELECTED}
+          `${foodsPathname ? 'bg-black' : 'bg-white'}
           flex
           items-center
           justify-center
-            `
+          rounded-full
+          h-9
+          w-9
+          shadow-sm
+          shadow-black
+          `
         }
         type="button"
         onClick={ () => navigate('/foods') }
       >
-        <GiKnifeFork data-testid="food-bottom-btn" className="text-2xl" />
+        <GiKnifeFork
+          data-testid="food-bottom-btn"
+          className={ `text-2xl ${foodsPathname ? 'text-white' : 'text-black'}` }
+        />
       </button>
     </footer>
   );
