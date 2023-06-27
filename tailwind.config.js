9e661e653521da30325764c8efa2a59fc57f2867
Halister Fernando <halisterfernando@hotmail.com>
2023-06-27 15:01:01 -0300

    chore: :art: animações personalizadas no tailwind - logo menor

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,22 @@ export default {
         "show-alert": {
           '0%': { opacity: 0, transform: 'translateY(100%)' },
           '100%': { opacity: 1, transform: 'translateY(0)' }
-        }
+        },
+        "slide-right": {
+          '0%': { opacity: 0, transform: 'translateX(100%)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' }
+        },
+        "slide-left": {
+          '0%': { opacity: 0, transform: 'translateX(-100%)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' }
+        } 
       },
       animation: {
         'slide-down': 'slide-down 1s ease-in-out',
         'slide-up': 'slide-up 1s ease-in-out',
-        'alert': 'slide-down 0.5s ease-in-out'
+        'alert': 'slide-down 0.5s ease-in-out',
+        'slide-right': 'slide-right 0.5s ease-in-out',
+        'slide-left': 'slide-left 1s ease-in-out'
       }
     },
   },
