c2e1a300b2de2d0bda6f99477fc5d586fd2b6ca3
Halister Fernando <halisterfernando@hotmail.com>
2023-07-03 14:32:26 -0300

    style: :art: responsividade do filtro de bebibas e comidas

--- src/components/MealAndDrinkFilter.jsx
+++ src/components/MealAndDrinkFilter.jsx
@@ -3,9 +3,9 @@ import propTypes from 'prop-types';
 
 export default function MealAndDrinkFilter({ filter }) {
   return (
-    <div className="flex justify-between p-2">
+    <div className="flex justify-between md:justify-evenly p-2">
       <button
-        className="bg-orange-400 rounded shadow-md shadow-black p-1 w-20 text-sm"
+        className="bg-orange-400 rounded-full shadow-md shadow-black p-1 w-20 text-sm"
         data-testid="filter-by-all-btn"
         name="All"
         onClick={ (ev) => filter(ev.target.name) }
@@ -13,7 +13,7 @@ export default function MealAndDrinkFilter({ filter }) {
         All
       </button>
       <button
-        className="bg-orange-400 rounded shadow-md shadow-black p-1 w-20 text-sm"
+        className="bg-orange-400 rounded-full shadow-md shadow-black p-1 w-20 text-sm"
         data-testid="filter-by-food-btn"
         name="Food"
         onClick={ (ev) => filter(ev.target.name) }
@@ -21,7 +21,7 @@ export default function MealAndDrinkFilter({ filter }) {
         Food
       </button>
       <button
-        className="bg-orange-400 rounded shadow-md shadow-black p-1 w-20 text-sm"
+        className="bg-orange-400 rounded-full shadow-md shadow-black p-1 w-20 text-sm"
         data-testid="filter-by-drink-btn"
         name="Drinks"
         onClick={ (ev) => filter(ev.target.name) }
