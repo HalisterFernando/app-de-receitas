c23f791d934342b7053457a343392f4f862995c5
Halister Fernando <halisterfernando@hotmail.com>
2023-08-01 17:41:43 -0300

    test: :monocle_face: testes da página de bebidas

---
+++ src/test/drink.test.jsx
@@ -0,0 +1,26 @@
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+
+describe('Food test suites', () => {
+    beforeEach(() => {
+      renderWithRouter(<App />, { route: '/drinks'})
+    })
+    it('should render header and footer', () => {
+       const pageTitle = screen.getByTestId('page-title');
+       const footer = screen.getByTestId('footer')
+
+       expect(pageTitle).toBeInTheDocument();
+       expect(pageTitle.textContent).toEqual('Drinks');
+       expect(footer).toBeInTheDocument();       
+    });
+    
+    it('should render 12 recipe cards', async () => {
+      await waitFor(() => {
+            const recipeCards = screen.getAllByTestId(/^[0-9]+-recipe-card$/);
+          expect(recipeCards).toHaveLength(12);            
+        }, {timeout: 2000});
+    });
+});
