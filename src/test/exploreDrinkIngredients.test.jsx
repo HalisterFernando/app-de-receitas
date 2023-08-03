e2126f09d3466456ebdef918356a3c14bc2ef418
Halister Fernando <halisterfernando@hotmail.com>
2023-08-03 12:14:41 -0300

    test: :monocle_face: testes para página de explorar bebidas e comidas por ingrediente

---
+++ src/test/exploreDrinkIngredients.test.jsx
@@ -0,0 +1,24 @@
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+
+describe('Explore drink ingredients test suites', () => {
+    beforeEach(() => {
+      renderWithRouter(<App />, { route: '/'})
+      fireEvent.change(screen.getByTestId('email-input'), { target: { value: 'test@example.com' } });
+      fireEvent.change(screen.getByTestId('password-input'), { target: { value: 'password123' } });
+      fireEvent.submit(screen.getByTestId('login-submit-btn'));
+      fireEvent.click(screen.getByTestId('explore-bottom-btn'));
+      fireEvent.click(screen.getByTestId('explore-drinks'));
+      fireEvent.click(screen.getByTestId('explore-by-ingredient'));
+    })
+    it('should render 12 ingredients', async () => {       
+        await waitFor(() => {          
+            const ingredients = screen.getAllByTestId(/^[\d]+-ingredient-card$/);
+            
+            expect(ingredients).toHaveLength(12);    
+        }, {timeout: 2000});
+    });
+});
